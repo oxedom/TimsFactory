cea365d9339e1790518b07dbc3fcf1178ff1737a
Sam <36157647+oxedom@users.noreply.github.com>
2022-02-04 12:12:04 +0200

    Fixed Start of Working year select

--- employeeMenu/editEmployee.js
+++ employeeMenu/editEmployee.js
@@ -4,9 +4,10 @@ let fname = document.getElementById("fname")
 let lname = document.getElementById("lname")
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('empid');
-
+let select = document.getElementById("StartOfWorkingYear");
 
 let form = document.getElementById("form")
+//put  request
 form.addEventListener("submit", (e) => 
 {
 e.preventDefault()
@@ -38,30 +39,32 @@ e.preventDefault()
 })
 
 
-
+//adds all the dates to the Start of Working year
 let year = new Date();
-for (let index = year.getFullYear() ; index > 1999; index--) {
-    let select = document.getElementById("StartOfWorkingYear");
+for (let index = year.getFullYear() ; index > 1998; index--) {
+   
     let option = document.createElement("option");
     option.text = index
     option.value = index
     select.appendChild(option)
-
 }
 
 
 fetch('https://localhost:44367/api/Department')
 .then(Response => Response.json())
 .then(data => {
-    let select = document.getElementById("deps");    
+    let selectDeps = document.getElementById("deps");    
 for (let index = 0; index < data.length; index++) 
 {
     let option = document.createElement("option");
 option.text = data[index].name;
 option.value = data[index].ID;
 selectDeps.appendChild(option);
-    
+
+
 }
+
+
 })
 
 
@@ -73,6 +76,18 @@ fetch(`https://localhost:44367/api/Employee/${myParam}`)
         fname.value = data.fname
         lname.value = data.lname
         selectDeps.value = data.DepartmentID
+        
+        console.log(data.startYear)
+        let options = document.querySelectorAll('option')
+        let longDateToYear = new Date(data.startYear)
+      
+        options.forEach(op => { 
+         
+            if(op.value == longDateToYear.getFullYear()) { op.setAttribute('selected', longDateToYear.getFullYear())}
+       
+            })
+
+
     })
 
 
